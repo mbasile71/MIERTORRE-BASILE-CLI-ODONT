a17ac34364af08ce02d6b25c244fe9dac00cc041
mbasile71 <basile_marcelo@yahoo.com.ar>
2023-11-07 21:01:41 -0300

    Commit 7-11-23 TRES

--- src/main/java/com/backend/clinicaodontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/backend/clinicaodontologica/ClinicaOdontologicaApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
@@ -12,7 +15,24 @@ public class ClinicaOdontologicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+		createDb();
 		logger.info("ClinicaOdontologica is now running...");
 	}
 
+	private static void createDb() {
+		Connection connection = null;
+		try {
+			Class.forName("org.h2.Driver");
+			connection = DriverManager.getConnection("jdbc:h2:~/bdcodontol;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
